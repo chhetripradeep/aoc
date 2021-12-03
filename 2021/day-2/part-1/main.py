8f454cac1c46ba9dc239ebd354687a6ab2462602
Pradeep Chhetri <pradeepchhetri4444@gmail.com>
2021-12-04 00:05:03 +0800

    2nd day

---
+++ 2021/day-2/part-1/main.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+def read_input(filename):
+    with open(filename) as fd:
+        return fd.read().strip()
+
+def compute_result(data):
+    horizontal = 0
+    depth = 0
+    for item in data:
+        if item[0] == "forward":
+            horizontal += int(item[1])
+        elif item[0] == "up":
+            depth -= int(item[1])
+        elif item[0] == "down":
+            depth += int(item[1])
+    return abs(horizontal) * abs(depth)
+
+def main():
+    data = []
+    for line in read_input("input.txt").split("\n"):
+        data.append(line.split())
+    print(compute_result(data))    
+
+if __name__ == '__main__':
+    main()
---
+++ 2021/day-2/part-2/main.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+def read_input(filename):
+    with open(filename) as fd:
+        return fd.read().strip()
+
+def compute_result(data):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for item in data:
+        if item[0] == "forward":
+            horizontal += int(item[1])
+            depth += aim * int(item[1])
+        elif item[0] == "up":
+            aim -= int(item[1])
+        elif item[0] == "down":
+            aim += int(item[1])
+    return abs(horizontal) * abs(depth)
+
+def main():
+    data = []
+    for line in read_input("input.txt").split("\n"):
+        data.append(line.split())
+    print(compute_result(data))    
+
+if __name__ == '__main__':
+    main()
